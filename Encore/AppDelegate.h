d4ace74377d78dd5f5a1456c7adeb997e031bc65
Miguel Tepale <miguel.tepale@gmail.com>
2017-05-04 17:57:30 -0400

    Initial commit

---
+++ Encore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Encore
+//
+//  Created by Miguel Tepale on 5/4/17.
+//  Copyright © 2017 Miguel Tepale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Encore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Encore
+//
+//  Created by Miguel Tepale on 5/4/17.
+//  Copyright © 2017 Miguel Tepale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
