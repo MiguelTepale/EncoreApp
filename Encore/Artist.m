5b1b0b2cf97e4f3de5b77f904a728f4f9ee9c9ef
BL <bcl168@gmail.com>
2017-05-05 12:25:37 -0400

    Add filled in SongListVC and fixed refresh bug in ResultsVC

--- Encore/Artist.m
+++ Encore/Artist.m
@@ -6,8 +6,70 @@
 //  Copyright © 2017 Miguel Tepale. All rights reserved.
 //
 
+
 #import "Artist.h"
+#import "Track.h"
+#import "Model.h"
+#import "SongListVC.h"
+
 
 @implementation Artist
+{
+    Model *_model;
+    Album *currentAlbum;
+    UINavigationController *_navigationController;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Method that execute the search for an album's track.
+//
+//////////////////////////////////////////////////////////////////////////////////////////
+- (void) findTracks:(long) albumIndex displayUnder:(UINavigationController *) controller
+{
+    currentAlbum = self.albumList[albumIndex];
+    _navigationController = controller;
+    
+    // Pass the request to the backend
+    [_model findTracks:currentAlbum.id];
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Method that implements the basic initializer.
+//
+//////////////////////////////////////////////////////////////////////////////////////////
+- (instancetype) init
+{
+    if (self = [super init])
+    {
+        _model = [Model sharedInstance];
+        _model.tracksDelegate = self;
+    }
+    
+    return self;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Method that is called by the Model to return the result of an album's track search.
+//
+//////////////////////////////////////////////////////////////////////////////////////////
+- (void) didFindTracks:(NSArray *)tracks
+{
+    // Initialize the track list view controller
+    SongListVC *controller = [[SongListVC alloc] init];
+    controller.title = currentAlbum.name;
+    controller.albumArtImageView.image = [UIImage imageWithData:currentAlbum.imageData];
+    controller.trackList = tracks;
+    
+    // Display the track list.
+    [_navigationController pushViewController:controller animated:YES];
+}
+
+- (void) didNotFindTracks:(NSString *)errorMsg
+{
+
+}
 
 @end
--- Encore/SongListVC.m
+++ Encore/SongListVC.m
@@ -6,32 +6,88 @@
 //  Copyright © 2017 Miguel Tepale. All rights reserved.
 //
 
+
 #import "SongListVC.h"
+#import "Track.h"
 
-@interface SongListVC ()
-
-@end
 
 @implementation SongListVC
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+
+    // Add a back button on the left side of the navigation bar
+    UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:@"←"
+                                                                   style:UIBarButtonItemStylePlain
+                                                                  target:self
+                                                                  action:@selector(performBackNavigation:)];
+    [backButton setTitleTextAttributes:@{ NSFontAttributeName:[UIFont systemFontOfSize:30] }
+                              forState:UIControlStateNormal];
+    self.navigationItem.leftBarButtonItem = backButton;
+
+    self.songTableView.delegate = self;
+    self.songTableView.dataSource = self;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+//////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Method is called when the user hit the back button.
+//
+//////////////////////////////////////////////////////////////////////////////////////////
+- (void) performBackNavigation:(id)sender
+{
+    // Exit current screen
+    [self.navigationController popViewControllerAnimated:NO];
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - UITableView Data Source Delegate Methods
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+//////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Delegate method to specify the number of section in the tableView.
+//
+//////////////////////////////////////////////////////////////////////////////////////////
+- (NSInteger) numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
 }
-*/
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Delegate method to specify the number of rows in the tableView.
+//
+//////////////////////////////////////////////////////////////////////////////////////////
+- (NSInteger) tableView:(UITableView *)tableView
+  numberOfRowsInSection:(NSInteger)section
+{
+    return self.trackList.count;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Delegate method for loading data into current row of the tableView.
+//
+//////////////////////////////////////////////////////////////////////////////////////////
+- (UITableViewCell *) tableView:(UITableView *)tableView
+          cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    
+    // Get a recycled table row
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    // If none are available then ...
+    if (nil == cell)
+        // Create a new row
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                      reuseIdentifier:CellIdentifier];
+    
+    Track *track = self.trackList[indexPath.row];
+    cell.textLabel.text = [[NSString alloc] initWithFormat:@"%d - %d   %@", track.discNumber, track.trackNumber, track.name];
+    
+    return cell;
+}
 
 @end
